4fb277cfcafc5c4b6a2c0b204e092dbb37761e01
bobbalu <53517761+Swethabobba123@users.noreply.github.com>
2019-10-13 20:04:22 +0530

    Update Dijkstras.c

--- Dijkstras.c
+++ Dijkstras.c
@@ -1,69 +1,52 @@
 #include<stdio.h>
-void dijkstras(int start,int cost[100][100],int dist[100],int n)
+void dijkstras(int start,int cost[20][20],int n,int dist[20])
 {
-int i,s[100],u,j,min,path[100],l=0;
+int s[20],l=0,path[20],min,i,j,u;
+for(i=1;i<=n;i++)
+s[i]=0;
+
+s[start]=1;
 path[++l]=start;
+for(j=1;j<=n;j++)
+{
+min=999;
 for(i=1;i<=n;i++)
 {
-s[i]=0;
-}
-  s[start]=1;
-  dist[start]=0;
-  for(j=1;j<=n;j++){
-  min=999;
-  for(i=1;i<=n;i++)
-  {
-  if(dist[i]<min && s[i]!=1){
-  min=dist[i];
+if(dist[i]<min && s[i]!=1){
+min=dist[i];
 u=i;
-  }
-  }
-  path[++l]=u;
-  s[u]=1;
-  for(i=1;i<=n;i++){
-  if(cost[u][i]<999 && s[i]==0)
-  {
-  if(dist[i]>dist[u]+cost[u][i])
-  dist[i]=dist[u]+cost[u][i];
- 
-  }
-  }
- }
+}
+}
+s[u]=1;
+path[++l]=u;
+for(i=1;i<=n;i++)
+{
+if(dist[i]>dist[u]+cost[u][i] && s[i]!=1 && cost[u][i]<999)
+dist[i]=dist[u]+cost[u][i];
+}
+}
+printf("\n path is : ");
+for(i=1;i<=n;i++)
+printf("%d ",path[i]);
+printf("\n distances from starting vertex to other vertices are : \n");
 for(i=1;i<=n;i++)
 printf("%d ",dist[i]);
-printf("\n");
-for(i=1;i<l;i++)
-printf("%d\t",path[i]);
-
-
 }
 
 
 int main()
 {
-int a[100][100],i,j,n,start,cost[100][100],dist[100];
-printf("enter the no. of nodes : ");
+int start,cost[20][20],dist[20],i,j,n;
+printf("enter no. of vertices:");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-for(j=1;j<=n;j++)
-{
-printf("%d-%d : ",i,j);
-scanf("%d",&a[i][j]);
-}
-}
-printf("enter starting element : ");
+printf("\nenter strating vertex: ");
 scanf("%d",&start);
-    for(i=1;i<=n;i++)
-{
+for(i=1;i<=n;i++)
 for(j=1;j<=n;j++)
-{
-cost[i][j]=a[i][j];
-}
-}
-   for(i=1;i<=n;i++)
-    dist[i]=cost[start][i];
-   dijkstras(start,cost,dist,n);
-   return 0;
-}
+scanf("%d",&cost[i][j]);
 
+for(i=1;i<=n;i++)
+dist[i]=cost[start][i];
+dijkstras(start,cost,n,dist);
+return 0;
+}
