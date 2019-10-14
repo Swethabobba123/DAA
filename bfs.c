66a6d4527f14bb60b759069912afd2939ef1fda6
bobbalu <53517761+Swethabobba123@users.noreply.github.com>
2019-10-14 10:25:55 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,38 +1,41 @@
 #include<stdio.h>
-#include<stdlib.h>
-int a[20][20],q[20];
-int vi[20],n,i,j,f=0,r=-1;
-void bfs(int v) {
-	for (i=1;i<=n;i++)
-	  if(a[v][i] && !vi[i])
-	   q[++r]=i;
-	if(f<=r) {
-		vi[q[f]]=1;
-		bfs(q[f++]);
-	}
+int q[15],f=0,r=-1;
+void bfs(int start,int n,int g[15][15],int vi[15])
+{
+int i;
+for(i=1;i<=n;i++)
+{
+if(g[start][i]==1 && vi[i]==0)
+{
+q[++r]=i;
 }
-int main() {
-	int v;
-	printf("\n Enter the number of vertices:");
-	scanf("%d",&n);
-	for (i=1;i<=n;i++) {
-		q[i]=0;
-		vi[i]=0;
-	}
-	printf("\n Enter graph data in matrix form:\n");
-	for (i=1;i<=n;i++)
-	  for (j=1;j<=n;j++)
-	   scanf("%d",&a[i][j]);
-	printf("\n Enter the starting vertex:");
-	scanf("%d",&v);
-	bfs(v);
-	printf("\n The node which are reachable are:\n");
-	for (i=1;i<=n;i++){
-	  if(vi[i])
-	   printf("%d\t",i);
-	  else
-	   printf("\n Bfs is not possible");
-	}
-	return 0;
 }
-
+if(f<=r)
+{
+vi[q[f]]=1;
+bfs(q[++f],n,g,vi);
+}
+}
+int main()
+{
+int i,j,start,n,g[15][15],vi[15];
+printf("enter no of nodes : ");
+scanf("%d",&n);
+printf("\nenter the adjacency matrix :\n");
+for(i=1;i<=n;i++)
+for(j=1;j<=n;j++)
+scanf("%d",&g[i][j]);
+printf("enter starting vertex :");
+scanf("%d",&start);
+for(i=1;i<=n;i++){
+vi[i]=0;
+q[i]=0;
+}
+q[++r]=start;
+bfs(start,n,g,vi);
+printf("\nFinal Answer\n");
+for(i=0;i<n;i++)
+printf("%d ",q[i]);
+printf("\n");
+return 0;
+}
